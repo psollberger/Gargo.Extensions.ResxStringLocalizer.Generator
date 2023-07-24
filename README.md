d29303f26dd6bc3fef65ef18cec5e5dcf96c6528
Patrick Sollberger <psollberger83@gmail.com>
2023-07-24 20:12:48 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# What is this?
+
+This is a source generator that creates Extension Methods for `IStringLocalizer<>` from resx files.
+
+To turn of creation of extension methods for a specific resx file, add `GenerateExtensionMethods=false` to the metadata of the resx file.
---
+++ src/Generator/ReferenceHandler.cs
@@ -0,0 +1,24 @@
+﻿namespace Gargo.Extensions.ResxStringLocalizer.Generator
+{
+  using System;
+  using System.Reflection;
+
+  internal static class ReferenceHandler
+  {
+    public static void Initialize()
+    {
+      AppDomain.CurrentDomain.AssemblyResolve += CurrentDomainOnAssemblyResolve;
+    }
+
+    private static Assembly CurrentDomainOnAssemblyResolve(object sender, ResolveEventArgs args)
+    {
+      if (args.Name.StartsWith("System.Resources.NetStandard, Version=1.1.0.0,"))
+        return Assembly.Load(References.References.System_Resources_NetStandard_dll);
+
+      if (args.Name.StartsWith("Microsoft.CodeAnalysis, Version=4.4.0.0,"))
+        return Assembly.Load(References.References.Microsoft_CodeAnalysis);
+
+      return null;
+    }
+  }
+}
